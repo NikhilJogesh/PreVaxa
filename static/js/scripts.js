e8735fa5ce5f5ae5d0c43fe97e5e91274988d70b
Nikhil <nikhiljogesh2007@gmail.com>
2025-08-07 23:52:54 +0530

    project template

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+// Custom JavaScript for PreVaxa
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("PreVaxa scripts loaded!");
+
+    // Example placeholder for future dynamic behavior:
+    // const statsBox = document.querySelector('.stats-box');
+    // statsBox.classList.add('highlight');
+});
